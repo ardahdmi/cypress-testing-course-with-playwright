a56a7451132359b5e73df7b7f340a4b4acb92c2c
Arda <ardahadimioglu@gmail.com>
2023-12-04 13:52:11 +0100

    adds user journey tests. networkidle

---
+++ tests/e2e/user-journey.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test"
+
+// 1. A user lands on the home page and finds one of the courses.
+// 2. They click on the “Get started” button and are taken to the course landing page.
+// 3. They click on the “Start Course” button and are taken to the first lesson of that course.
+// 4. They read the lesson and complete the quiz at the bottom.
+// 5. After answering the quiz correctly, they click on the “Next Lesson” button and are taken to the next lesson.
+
+test.describe.only("User Journey", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000")
+  })
+
+  test("a user can find a course on the home page and complete the courses lessons", async ({
+    page,
+  }) => {
+    await page
+      .locator("[data-test=course-0]")
+      .getByRole("link", { name: "Get started" })
+      .click()
+
+    expect(page.url()).toBe(
+      "http://localhost:3000/testing-your-first-application"
+    )
+
+    await page.locator("[data-test=next-lesson-button]").click()
+    // TODO fails without networkidle
+    await page.waitForLoadState("networkidle")
+    expect(page.url()).toBe(
+      "http://localhost:3000/testing-your-first-application/app-install-and-overview"
+    )
+
+    await page.locator("[data-test=challenge-answer-0]").click()
+    expect(page.locator("[data-test=next-lesson-button]")).toBeVisible()
+    await page.locator("[data-test=next-lesson-button]").click()
+
+    expect(page.url()).toBe(
+      "http://localhost:3000/testing-your-first-application/installing-cypress-and-writing-our-first-test"
+    )
+
+    await page.locator("[data-test=challenge-answer-0]").click()
+    expect(page.locator("[data-test=next-lesson-button]")).toBeVisible()
+    await page.locator("[data-test=next-lesson-button]").click()
+
+    expect(page.url()).toBe(
+      "http://localhost:3000/testing-your-first-application/setting-up-data-before-each-test"
+    )
+
+    await page.locator("[data-test=challenge-answer-0]").click()
+    expect(page.locator("[data-test=next-lesson-button]")).toBeVisible()
+    await page.locator("[data-test=next-lesson-button]").click()
+
+    expect(page.url()).toBe("http://localhost:3000/")
+  })
+})
