e57699c22337d75ac9c16cd79cddd668c0d3ce28
Arda <ardahadimioglu@gmail.com>
2023-12-06 11:15:12 +0100

    adds multiple todo adding test

--- tests/e2e/todo-app.spec.ts
+++ tests/e2e/todo-app.spec.ts
@@ -13,5 +13,21 @@ test.describe.only("React MVC App", () => {
 
     // Step 3: Expect
     await expect(page.locator(".todo-list li")).toHaveCount(1)
+    await expect(page.locator(".todo-list li label")).toHaveText("Buy tomatoes")
+  })
+
+  test("Adding multiple todos", async ({ page }) => {
+    // Step 2: Act
+    const items = ["Buy tomatoes", "Cook dinner", "Clean office desk"]
+
+    for (const item of items) {
+      await page.locator(".new-todo").fill(item)
+      await page.keyboard.press("Enter")
+    }
+
+    // Step 3: Expect
+    const getTodoList = () => page.locator(".todo-list li")
+    await expect(getTodoList()).toHaveCount(3)
+    await expect(getTodoList()).toHaveText(items)
   })
 })
